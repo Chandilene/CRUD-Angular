67e7dab245d4ec1587c6d2039f0af609ca17473f
Chandilene <chandileneborges@gmail.com>
2024-08-19 22:39:07 -0300

    feat:Implementing validators, FormGroup, tables, and creating the Pessoa class

--- Crud/src/app/user-form-component/user-form-component.component.ts
+++ Crud/src/app/user-form-component/user-form-component.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-user-form-component',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './user-form-component.component.html',
   styleUrl: './user-form-component.component.css',
 })
 export class UserFormComponentComponent {
-  nome: string;
-  idade: number = null;
+  //objeto de formulario
+  formulario = new FormGroup({
+    nome: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    idade: new FormControl(null, [
+      Validators.required,
+      Validators.min(0),
+      Validators.max(120),
+    ]),
+    cidade: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  });
 }
